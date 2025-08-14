38310c9558f25aa92fe33c30eb4db38c26eefd68
Pasindu Jayalal <PD294@canterbury.ac.uk>
2025-08-14 14:09:34 +0100

    use StratifiedKFold to test the model's performance on each split

--- src/train_sms.py
+++ src/train_sms.py
@@ -3,12 +3,16 @@ from src.pipelines import make_pipeline
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
 
 
 def main():
+    
+    print("Training SMS spam baseline (LinearSVC) ...")
     X, y = load_dataset("data/SMSspam2.csv")
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
@@ -16,8 +20,11 @@ def main():
 
     clf = Pipeline([
         ('preprocess', make_pipeline(clean_text_list, preprocess_text_list)),
-        ('model', LinearSVC(class_weight="balanced", C=1.0))
+        ('model', LinearSVC(class_weight="balanced", C=1.0 ,random_state=42))
     ])
+    
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    scores = cross_val_score(clf, X_train, y_train, cv=cv, scoring="f1", n_jobs=-1)
 
     # pipe = make_pipeline(clean_text_list, preprocess_text_list)
 
@@ -36,6 +43,9 @@ def main():
     cm = confusion_matrix(y_test, y_pred)
     cm_df = pd.DataFrame(cm, index=['Actual: Not Spam', 'Actual: Spam'], columns=['Predicted: Not Spam', 'Predicted: Spam'])
     print(cm_df)
+    
+    print("F1 (5-fold CV):", np.round(scores, 4))
+    print("Mean ± Std:", np.round(scores.mean(), 4), "±", np.round(scores.std(), 4))
 
 if __name__ == "__main__":
     main()
