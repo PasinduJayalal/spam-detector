4fd8d3e41e1703405e51108817d78aaf41bb6cd5
Pasindu Jayalal <PD294@canterbury.ac.uk>
2025-09-15 13:29:10 +0100

    Remove smoke tests that depended on local file paths

--- tests/smoke/test_pipelines.py
+++
@@ -1,7 +0,0 @@
-from src.utils import clean_text_list, preprocess_text_list, load_dataset
-from src.pipelines import make_pipeline
-
-X, y = load_dataset(r"D:\Professional\SpamDetector\data\SMSSpamCollection")
-pipe = make_pipeline(clean_text_list, preprocess_text_list)
-X_vec = pipe.fit_transform(X)
-print("Matrix shape:", X_vec.shape)
